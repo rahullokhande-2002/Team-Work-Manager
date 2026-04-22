04ad758b992994d11df5b2bda408c410cd2bedb7
rahul <lokhanderahul2002@gmail.com>
2026-04-22 17:11:38 +0530

    ready for deployemnt

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-# Build script for Render deployment
 
-# Install Python dependencies
 pip install -r requirements.txt
-
-# Run database migrations
-python manage.py migrate
-
-# Collect static files
 python manage.py collectstatic --noinput
+python manage.py migrate
